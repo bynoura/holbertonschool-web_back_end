cab5c73bda1f0d75027dfaaac0917832387baa36
bynoura <nr.abdullah06@gmail.com>
2026-02-17 09:09:25 +0300

    Update 2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
+"""Docstring for python_variable_annotations.2-floor"""
+import math
 
+
+def floor(n: float) -> int:
+    """Docstring for floor"""
+    return math.floor(n)
