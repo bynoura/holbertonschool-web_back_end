36f8321c7ea6749953a1b91d185492ca9ab4c290
bynoura <nr.abdullah06@gmail.com>
2026-02-17 09:09:44 +0300

    Update 3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+"""Docstring for python_variable_annotations.3-to_str"""
 
+
+def to_str(n: float) -> str:
+    """this function return string format from float format"""
+    return str(n)
